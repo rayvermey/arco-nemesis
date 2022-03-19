fbcf364fcec265684dfe2d5219a089240f815570
Ray Vermey <rayvermey@gmail.com>
2022-03-19 15:56:01 +0100

    Initial

---
+++ Personal/960-ckb-next.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website	: https://www.arcolinux.info
+# Website	: https://www.arcolinux.com
+# Website	: https://www.arcolinuxd.com
+# Website	: https://www.arcolinuxb.com
+# Website	: https://www.arcolinuxiso.com
+# Website	: https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+if [ ! -f /usr/bin/hwinfo ]; then
+  sudo pacman -S --noconfirm --needed hwinfo
+fi
+
+if hwinfo | grep "CORSAIR K70" > /dev/null 2>&1 ; then
+
+	echo
+	tput setaf 2
+	echo "################################################################"
+	echo "################### Corsair keyboard to be installed"
+	echo "################################################################"
+	tput sgr0
+	echo
+
+	sudo pacman -S --noconfirm --needed ckb-next-git
+	installed_dir=$(dirname `pwd`)
+	[ -d $HOME"/.config/ckb-next" ] || mkdir -p $HOME"/.config/ckb-next"
+	cp -r $installed_dir/Personal/settings/ckb-next/ ~/.config/
+	sudo systemctl enable ckb-next-daemon
+	sudo systemctl start ckb-next-daemon
+
+	cp -f $installed_dir/settings/ckb-next/*.desktop $HOME"/.config/autostart"
+
+	echo
+	tput setaf 2
+	echo "################################################################"
+	echo "################### Corsair keyboard installed"
+	echo "################################################################"
+	tput sgr0
+	echo
+
+fi
